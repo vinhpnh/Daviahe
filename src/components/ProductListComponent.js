d7914ec7852e53a869ab6bc5e670967733989e31
volinhkhanh <vo.khanh1104@gmail.com>
2019-09-08 16:30:38 +0700

    Update ProductListComponent.js

--- src/components/ProductListComponent.js
+++ src/components/ProductListComponent.js
@@ -30,6 +30,58 @@ export default class AddressMap extends Component {
                         </div>
                     </div>
                 </div>
+                <div className="row">
+                    <div className="col-sm">
+                        <div className="product-item">
+                            <img className="product-item-img" src="img/gach1.jpg" alt="First image" />
+                            <div className="product-item-title">Gach via he so 1</div>
+                        </div>
+                    </div>
+                    <div className="col-sm">
+                        <div className="product-item">
+                            <img className="product-item-img" src="img/gach1.jpg" alt="First image" />
+                            <div className="product-item-title">Gach via he so 1</div>
+                        </div>
+                    </div>
+                    <div className="col-sm">
+                        <div className="product-item">
+                            <img className="product-item-img" src="img/gach1.jpg" alt="First image" />
+                            <div className="product-item-title">Gach via he so 1</div>
+                        </div>
+                    </div>
+                    <div className="col-sm">
+                        <div className="product-item">
+                            <img className="product-item-img" src="img/gach1.jpg" alt="First image" />
+                            <div className="product-item-title">Gach via he so 1</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm">
+                        <div className="product-item">
+                            <img className="product-item-img" src="img/gach1.jpg" alt="First image" />
+                            <div className="product-item-title">Gach via he so 1</div>
+                        </div>
+                    </div>
+                    <div className="col-sm">
+                        <div className="product-item">
+                            <img className="product-item-img" src="img/gach1.jpg" alt="First image" />
+                            <div className="product-item-title">Gach via he so 1</div>
+                        </div>
+                    </div>
+                    <div className="col-sm">
+                        <div className="product-item">
+                            <img className="product-item-img" src="img/gach1.jpg" alt="First image" />
+                            <div className="product-item-title">Gach via he so 1</div>
+                        </div>
+                    </div>
+                    <div className="col-sm">
+                        <div className="product-item">
+                            <img className="product-item-img" src="img/gach1.jpg" alt="First image" />
+                            <div className="product-item-title">Gach via he so 1</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
