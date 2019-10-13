3b35e1687ea096c38c8f0a1d37a8a421c8c3e6d3
volinhkhanh <vo.khanh1104@gmail.com>
2019-10-13 09:38:43 +0700

    update input and button in contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,32 +22,30 @@ export default class Contact extends Component {
                                     <p className="notice-help" align="center">Quý khách có thể liên hệ với chúng tôi từ website này bằng cách điền đầy đủ những thông tin vào form liên hệ bên dưới. Chúng tôi sẽ liên hệ với Quý khách trong thời gian sớm nhất. Xin chân thành cảm ơn.</p>
                                     <form action="" method="post" className="contact-form" novalidate="novalidate">
                                         <div className="form-group">
-                                            <input className="input-form" placeholder="Họ và tên" type="text" name="your-name" size="40" aria-required="true" aria-invalid="false" />
+                                            <input className="form-control" placeholder="Họ và tên" type="text" name="your-name" size="40" aria-required="true" aria-invalid="false" />
                                             <span className="field-required">*</span>
                                         </div>
                                         <div className="form-group">
-                                            <input className="input-form" placeholder="Địa chỉ" type="text" name="your-address" size="40" aria-required="true" aria-invalid="false" />
+                                            <input className="form-control" placeholder="Địa chỉ" type="text" name="your-address" size="40" aria-required="true" aria-invalid="false" />
                                             <span className="field-required">*</span>
                                         </div>
                                         <div className="form-group">
-                                            <input className="input-form" placeholder="Số  điện thoại" type="text" name="your-phone" size="40" aria-required="true" aria-invalid="false" />
+                                            <input className="form-control" placeholder="Số  điện thoại" type="text" name="your-phone" size="40" aria-required="true" aria-invalid="false" />
                                             <span className="field-required">*</span>
                                         </div>
                                         <div className="form-group">
-                                            <input className="input-form" placeholder="Email" type="email" name="your-email" size="40" aria-required="true" aria-invalid="false" />
+                                            <input className="form-control" placeholder="Email" type="email" name="your-email" size="40" aria-required="true" aria-invalid="false" />
                                                 <span className="field-required">*</span>
                                         </div>
                                         <div className="form-group">
-                                            <input className="input-form" placeholder="Chủ đề" type="text" name="your-subject" size="40" aria-required="true" aria-invalid="false" />
+                                            <input className="form-control" placeholder="Chủ đề" type="text" name="your-subject" size="40" aria-required="true" aria-invalid="false" />
                                             <span className="field-required">*</span>
                                         </div>
                                         <div className="form-group">
-                                            <textarea className="input-form" placeholder="Nội dung" name="your-message" cols="40" rows="10" aria-required="true" aria-invalid="false" />
+                                            <textarea className="form-control" placeholder="Nội dung" name="your-message" cols="40" rows="10" aria-required="true" aria-invalid="false" />
                                             <span className="field-required">*</span>
                                         </div>
-                                        <div className="form-group">
-                                            <input type="submit" value="Gửi" className="btn-submit" />
-                                        </div>
+                                        <button type="submit" className="btn btn-success">Gửi thông tin</button>
                                     </form>
                                 </div>
                             </div>
